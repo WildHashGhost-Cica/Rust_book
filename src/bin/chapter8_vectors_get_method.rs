cbbdbbd19bde245dc8e6979f7c189cf2f5cceea8
WildHashGhost-Cica <wildhashghost@gmail.com>
2022-10-30 17:15:07 +0000

    vectors

--- src/bin/chapter8_vectors_get_method.rs
+++ src/bin/chapter8_vectors_get_method.rs
@@ -1,8 +1,11 @@
 fn main(){
-    let v= vec![1,2,3];
+    let mut v= vec![1,2,3];
+
+    v.push(6); //we can't add extra element after when we borred 
 
     let third = &v[2];
-    //println!("The third element is {}", third);
+    
+    println!("The third element is {}", third);
     
     match v.get(2){
         Some(third)=> println!("This element is {}", third),
