ec8e030b5d7e4e47b71af54e6bd6f4d270f3e943
WildHashGhost-Cica <wildhashghost@gmail.com>
2022-10-30 16:55:40 +0000

    vector get method

---
+++ src/bin/chapter8_vectors_get_method.rs
@@ -0,0 +1,11 @@
+fn main(){
+    let v= vec![1,2,3];
+
+    let third = &v[2];
+    //println!("The third element is {}", third);
+    
+    match v.get(2){
+        Some(third)=> println!("This element is {}", third),
+        None => println!("There is no third element"),
+    }
+}
