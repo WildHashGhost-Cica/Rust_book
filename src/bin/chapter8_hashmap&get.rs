b872ab5cd1fc62411ea5dde7c718268dfcbb07bb
WildHashGhost-Cica <wildhashghost@gmail.com>
2022-11-01 15:49:09 +0000

    hashmap and get

---
+++ src/bin/chapter8_hashmap&get.rs
@@ -0,0 +1,18 @@
+use std::collections:HashMap;
+
+fn main(){
+    let blue = String::from("Blue");
+    let yellow = String::from("Yellow");
+
+    let mut scores = HashMap::new();
+
+    scores.insert(blue, 10);
+    scores.insert(yellow, 50);
+
+    let team_name = String::from("Blue");
+    let score = scores.get(&team_name);
+
+    for (key, value) in &scores {
+        println!("{} :{}", key, value);
+    }
+}
